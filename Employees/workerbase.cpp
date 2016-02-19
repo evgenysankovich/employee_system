1c28d7bfa19f25c3e1215f7a5bd19bdc6dc73db7
daizo <daizo@ya.ru>
2016-02-19 17:08:56 +0200

    Создан базовый класс для работников где будет высчитываться зарплата. + метод сколько лет работает сотрудник.

---
+++ Employees/workerbase.cpp
@@ -0,0 +1,23 @@
+#include "workerbase.h"
+
+WorkerBase::WorkerBase(QObject *parent) : QObject(parent)
+{
+
+}
+
+WorkerBase::~WorkerBase()
+{
+
+}
+
+int WorkerBase::timeWorkYear(QDateTime hireDate, QDateTime currentDate = QDateTime::currentDateTime())
+{
+    QDateTime tempDate;
+    tempDate.setTime_t(currentDate.toTime_t()-hireDate.toTime_t());
+    tempDate = tempDate.addYears(-1970);
+    if (tempDate.toString("yy").toInt()) {
+        return tempDate.toString("yy").toInt();
+    } else {
+        return 0;
+    }
+}
---
+++ Employees/workerbase.h
@@ -0,0 +1,25 @@
+#ifndef WORKERBASE_H
+#define WORKERBASE_H
+
+#include <QObject>
+#include <database.h>
+
+class WorkerBase : public QObject
+{
+    Q_OBJECT
+public:
+    explicit WorkerBase(QObject *parent = 0);
+    virtual ~WorkerBase();
+
+signals:
+
+public slots:
+
+public:
+    int timeWorkYear(QDateTime, QDateTime);
+    virtual double salary();
+    virtual double recursionSubordinate(double) = 0;
+
+};
+
+#endif // WORKERBASE_H
